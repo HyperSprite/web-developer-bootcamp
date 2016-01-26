ed01a102a50a83206844513c902fbfd03d531547
HyperSprite <chris-github@hypersprite.com>
2016-01-25 20:10:24 -0800

    To-Do List, real end of lecture163

--- public/projects/assets/js/lecture160.js
+++ public/projects/assets/js/lecture160.js
@@ -1,1 +1,21 @@
-lecture160.js
+// lecture160.js
+
+// check off specific todos by clicking
+// $('li').click(function() {
+//   if ($(this).css('color') === 'rgb(128, 128, 128)') {
+//     $(this).css({
+//       textDecoration: 'none',
+//       color: 'black',
+//     });
+//   } else {
+//     $(this).css({
+//       textDecoration: 'line-through',
+//       color: 'gray',
+//     });
+//   }
+// });
+
+$('li').click(function() {
+  $(this).toggleClass('completed');
+});
+
