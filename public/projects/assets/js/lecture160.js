a14e8b4d4c91b0247a4aa1f151a5e146ffa2cd37
HyperSprite <chris-github@hypersprite.com>
2016-01-25 21:58:25 -0800

    To-Do List, end of lecture166

--- public/projects/assets/js/lecture160.js
+++ public/projects/assets/js/lecture160.js
@@ -1,21 +1,34 @@
 // lecture160.js
 
-// check off specific todos by clicking
-// $('li').click(function() {
-//   if ($(this).css('color') === 'rgb(128, 128, 128)') {
-//     $(this).css({
-//       textDecoration: 'none',
-//       color: 'black',
-//     });
-//   } else {
-//     $(this).css({
-//       textDecoration: 'line-through',
-//       color: 'gray',
-//     });
-//   }
-// });
-
-$('li').click(function() {
+// we need to go up to an element that is on the page
+// when jQuery loads and then back down to the li
+$('ul').on('click', 'li', function() {
+  // we are adding the class that
+  // does the line-through and gray
   $(this).toggleClass('completed');
 });
 
+$('ul').on('click', 'span', function(event) {
+  // here we are doing a fadeOut at half a second
+  // there is a callback function that will fire after
+  // we are done with the fade
+  $(this).parent().fadeOut(500, function() {
+    $(this).remove();
+  });
+  // this keeps our click from bubling up
+  event.stopPropagation();
+});
+
+$('input[type="text"').keypress(function(event) {
+  if (event.which === 13) {
+// HyperSprite: added trim to remove white space on either side of the todo
+    var todoText = $(this).val().trim();
+// HyperSprite: checking to make sure there is a todo
+// so we don't make an empty todos by accident
+      $(this).val('');
+    if (todoText) {
+      // adding the new li with span and text
+      $('ul').append('<li><span>X</span> ' + todoText + '</li>');
+    }
+  }
+});
