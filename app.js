b29c03d739bb2bd236031a8aa663a35d480cbc65
HyperSprite <chris-github@hypersprite.com>
2016-01-31 22:02:12 -0800

    YelpCamp - up to lecture207

--- app.js
+++ app.js
@@ -10,13 +10,6 @@ app.use(express.static('public'));
 app.use(bodyParser.urlencoded({extended: true}));
 app.set('view engine', 'ejs');
 
-
-
-app.get('/', function(req, res) {
-  res.status(200).render('index');
-});
-
-app.get('/campgrounds', function(req, res) {
   var campgrounds = [
     {name: 'Camp Coffee', image: 'https://images.unsplash.com/photo-1414016642750-7fdd78dc33d9'},
     {name: 'Rocky Vista', image: 'https://images.unsplash.com/uploads/14131591236686822d392/95b6ec26'},
@@ -27,8 +20,30 @@ app.get('/campgrounds', function(req, res) {
     {name: 'Vista View Valley', image: 'https://images.unsplash.com/photo-1418225043143-90858d2301b4'},
     {name: 'Grass Lands Camp Ground', image: 'https://images.unsplash.com/photo-1445207966278-0a0a65a2047b'},
   ];
-  console.dir(campgrounds);
+
+app.get('/', function(req, res) {
+  res.status(200).render('index');
+});
+
+app.get('/campgrounds', function(req, res) {
   res.status(200).render('campgrounds', {campgrounds: campgrounds});
+});
+
+app.get('/campgrounds/new', function(req, res) {
+  res.status(200).render('campgrounds-new');
+});
+
+app.post('/campgrounds', function(req, res) {
+  var data = {
+    name: req.body.name,
+    image: req.body.image,
+  };
+  campgrounds.push(data);
+  res.status(302).redirect('/campgrounds');
+});
+
+app.get('*', function(req, res) {
+  res.status(404).send('Error 404: Page not found');
 });
 
 app.listen(appPORT, appIP, function() {
