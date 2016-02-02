7936d62cf19da9fdaf97add984280e2dd15b6300
HyperSprite <chris-github@hypersprite.com>
2016-02-01 21:13:49 -0800

    YelpCamp v2 - up to lecture215

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 // app.js for yelpcamp
 const express = require('express');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb://localhost/yelpcamp/');
 
 const app = express();
 const appIP = process.env.IP || '127.0.0.1';
@@ -10,31 +13,28 @@ app.use(express.static('public'));
 app.use(bodyParser.urlencoded({extended: true}));
 app.set('view engine', 'ejs');
 
-  var campgrounds = [
-    {name: 'Olmsted Point', image: 'http://hypersprite.com/blog/wp-content/uploads/2013/08/HS_6971-Edit.jpg'},
-    {name: 'Bodie Ghost Town', image: 'http://hypersprite.com/blog/wp-content/uploads/2013/08/HS_6250_HDR_ps.jpg'},
-    {name: 'Hetch Hetchy  Reservoir', image: 'http://hypersprite.com/blog/wp-content/uploads/2012/08/HS0417_18_19_20.jpg'},
-    {name: 'Middle of Lombard Street', image: 'http://hypersprite.com/blog/wp-content/uploads/2011/05/MG_0879.jpg'},
-    {name: 'Olmsted Point', image: 'http://hypersprite.com/blog/wp-content/uploads/2013/08/HS_6971-Edit.jpg'},
-    {name: 'Bodie Ghost Town', image: 'http://hypersprite.com/blog/wp-content/uploads/2013/08/HS_6250_HDR_ps.jpg'},
-    {name: 'Hetch Hetchy  Reservoir', image: 'http://hypersprite.com/blog/wp-content/uploads/2012/08/HS0417_18_19_20.jpg'},
-    {name: 'Middle of Lombard Street', image: 'http://hypersprite.com/blog/wp-content/uploads/2011/05/MG_0879.jpg'},
-    {name: 'Olmsted Point', image: 'http://hypersprite.com/blog/wp-content/uploads/2013/08/HS_6971-Edit.jpg'},
-    {name: 'Bodie Ghost Town', image: 'http://hypersprite.com/blog/wp-content/uploads/2013/08/HS_6250_HDR_ps.jpg'},
-    {name: 'Hetch Hetchy  Reservoir', image: 'http://hypersprite.com/blog/wp-content/uploads/2012/08/HS0417_18_19_20.jpg'},
-    {name: 'Middle of Lombard Street', image: 'http://hypersprite.com/blog/wp-content/uploads/2011/05/MG_0879.jpg'},
-    {name: 'Olmsted Point', image: 'http://hypersprite.com/blog/wp-content/uploads/2013/08/HS_6971-Edit.jpg'},
-    {name: 'Bodie Ghost Town', image: 'http://hypersprite.com/blog/wp-content/uploads/2013/08/HS_6250_HDR_ps.jpg'},
-    {name: 'Hetch Hetchy  Reservoir', image: 'http://hypersprite.com/blog/wp-content/uploads/2012/08/HS0417_18_19_20.jpg'},
-    {name: 'Middle of Lombard Street', image: 'http://hypersprite.com/blog/wp-content/uploads/2011/05/MG_0879.jpg'},
-  ];
+
+// schema setup
+const campgroundSchema = new mongoose.Schema({
+  name: 'String',
+  image: 'String',
+});
+
+const Campground = mongoose.model('Campground', campgroundSchema);
+
 
 app.get('/', function(req, res) {
   res.status(200).render('index');
 });
 
 app.get('/campgrounds', function(req, res) {
-  res.status(200).render('campgrounds', {campgrounds: campgrounds});
+  Campground.find({}, function(err, campgrounds) {
+    if (err) {
+      console.log(err);
+    } else {
+      res.status(200).render('campgrounds', {campgrounds: campgrounds});
+    }
+  });
 });
 
 app.get('/campgrounds/new', function(req, res) {
@@ -46,8 +46,14 @@ app.post('/campgrounds', function(req, res) {
     name: req.body.name,
     image: req.body.image,
   };
-  campgrounds.push(data);
-  res.status(302).redirect('/campgrounds');
+  Campground.create(data, function(err, cb) {
+    if (err) {
+      console.log(err);
+    } else {
+      console.log(cb);
+      res.status(302).redirect('/campgrounds');
+    }
+  });
 });
 
 app.get('*', function(req, res) {
